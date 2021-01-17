2bf35f3800d4830585cbef2584f57b8364e10a59
Laura Aupert <aupert.laura@gmail.com>
2021-01-17 18:08:42 -0500

    With last touchups and a readme for demo

--- README.js
+++ README.js
@@ -41,6 +41,7 @@ inquirer
             choices: [
                  "MIT",
                  "Apache",
+                 "ISC",
                  "None"
             ]
         },
@@ -64,11 +65,11 @@ inquirer
     ])
         .then((response) => {
         const badge = 
-        response.license === "MIT" ? `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)` : ``
-        || response.license === "Apache" ? `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)` : ``
+            response.license === "MIT" ? `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)` : ``
+            || response.license === "Apache" ? `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)` : ``
  
         const title = `
-# ${response.title} ${badge}
+# ${response.title}\t${badge}
 
 ## DESCRIPTION
 
@@ -116,7 +117,7 @@ ${response.usage}
     }
 
     const contributing = () => {
-        if (response.contributing) {
+        if (response.contributions) {
             return ` 
 ## CONTRIBUTIONS <a name="CONTRIBUTIONS"></a>
 
@@ -193,6 +194,7 @@ SOFTWARE.`
         } else if (response.license === "Apache") {
             return `
 ## LICENSE
+Apache License 2.0
 Copyright 2021 ${response.github}
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -206,11 +208,28 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.`
+        } else if (response.license === 'ISC') {
+            return `
+## LICENSE
+ISC License (ISC)
+
+Copyright (c) 2021 ${response.github}
+
+Permission to use, copy, modify, and/or distribute this software for any purpose
+with or without fee is hereby granted, provided that the above copyright notice 
+and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD 
+TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO 
+EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL 
+DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER 
+IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
+CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.`   
         } else if (response.license === "None") {
             return ``
         }
     }
-       
+      
     let total = title + table() + installation() + usage() + contributing() + testing() + questions() + license()
 
     fs.writeFile('./assets/README.md', total, (e) => {
